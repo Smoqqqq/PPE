79aff9891d6af5651700ce7ee1478593cb5d9302
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-08 13:21:00 +0200

    Update addBooks.php

--- functions/addBooks.php
+++ functions/addBooks.php
@@ -20,7 +20,7 @@ $desc = $_POST['desc'];
 $submitBook = $_POST['submitBook'];
 if($submitBook){
     if(!empty($title) && !empty($desc) && !empty($cover)){
-        $query_register = "INSERT INTO `books` (`book_id`, `title`, `description`, `cover`) VALUES (NULL, '$title', addslashes($desc), addslashes($cover))";
+        $query_register = "INSERT INTO `books` (`book_id`, `title`, `description`, `cover`) VALUES (NULL, '$title', 'addslashes($desc)', 'addslashes($cover)')";
         mysqli_query($conn, $query_register);
     }else{
         echo "<e class='erreur'>Formulaire mal rempli</e>";
